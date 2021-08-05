072a35081ae26f2ff41102a7396bba24d3d47fe5
김은지 <a_sldkfj@naver.com>
2021-08-05 13:34:22 +0900

    [Update] 바닥 장식

--- 김은지/210811/Main_1388_바닥장식.java
+++ 김은지/210811/Main_1388_바닥장식.java
@@ -22,19 +22,21 @@ public class Main_1388_바닥장식 {
 		
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (floor[i][j] == '-' || floor[i][j] == '|') dfs(floor[i][j], i, j, false);
+				if (floor[i][j] == '-' || floor[i][j] == '|') {
+					dfs(floor[i][j], i, j);
+					cnt++;
+				}
 			}
 		}
 		
 		System.out.println(cnt);
 	}
 	
-	static void dfs(char c, int x, int y, boolean flag) {
+	static void dfs(char c, int x, int y) {
 		if (0<=x && x<N && 0<=y && y<M && c == floor[x][y]) {
-			if (!flag) cnt++;
 			floor[x][y] = '0';
-			if (c == '-') dfs(c, x, y+1, true);
-			if (c == '|') dfs(c, x+1, y, true);
+			if (c == '-') dfs(c, x, y+1);
+			if (c == '|') dfs(c, x+1, y);
 		} else return;
 	}
 
